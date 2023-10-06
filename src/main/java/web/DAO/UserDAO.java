385fc393cf344b1794944ea4613f01636159dfed
Chester004 <br_nsd@mail.ru>
2023-10-06 14:17:52 +0300

    Изменил @Component на @Repository

--- src/main/java/web/DAO/UserDAO.java
+++ src/main/java/web/DAO/UserDAO.java
@@ -1,14 +1,14 @@
 package web.DAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.models.User;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDAO {
 
     @Autowired
